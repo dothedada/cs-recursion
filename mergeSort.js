a692dd1856858b17f5d603c5173cedd6770d5ae4
Miguel Mejía <121742455+dothedada@users.noreply.github.com>
2024-05-12 11:58:51 -0500

    Crea la solucion al ejercicio de mergeSort

---
+++ mergeSort.js
@@ -0,0 +1,30 @@
+const mergeSort = list => {
+	if (list.length > 1) {
+		// Divide the array and introduce the recursive function
+		const midPoint = Math.ceil(list.length / 2)
+		const left = mergeSort(list.slice(0, midPoint))
+		const right = mergeSort(list.slice(midPoint))
+
+		// loop over the parts values to create the new array sorted
+		const sortedArray = []
+		while (left.length && right.length) {
+			if (left[0] <= right[0]) {
+				sortedArray.push(left.shift())
+			} else {
+				sortedArray.push(right.shift())
+			}
+		}
+		// if remains any value in the left o rigth array
+		if (left.length) sortedArray.push(...left)
+		if (right.length) sortedArray.push(...right)
+
+		return sortedArray
+	}
+
+	return list
+}
+
+const arr = [3, 2, 14, 13, 8, 5, 1, 0]
+const arr2 = [105, 79, 100, 110, 12, 23498, 123]
+console.log(mergeSort(arr))
+console.log(mergeSort(arr2))
