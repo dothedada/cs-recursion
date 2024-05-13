2d87afeb64c0f9054ce6314de675a74c0f7c81c9
Miguel Mejía <121742455+dothedada@users.noreply.github.com>
2024-05-13 10:24:40 -0500

    Icluye la versión iterativa de Binary search

--- binarySearch.js
+++ binarySearch.js
@@ -9,8 +9,26 @@
 const binarySearch = (arr, element, index = 0) => {
 	const midPoint = Math.floor(arr.length / 2)
 	if (arr[midPoint] > element) return binarySearch(arr.slice(0, midPoint), element, index)
-	if (arr[midPoint] < element) return binarySearch(arr.slice(midPoint), element, midPoint+ index)
+	if (arr[midPoint] < element) return binarySearch(arr.slice(midPoint), element, midPoint + index)
 	if (arr[midPoint] === element) return `índice ${index + midPoint}, ${element}.`
+	return `No se encontró ${element}`
+}
+
+const binarySearchIterative = (arr, element) => {
+	let low = 0
+	let high = arr.length - 1
+
+	while (low <= high) {
+		let midPoint = Math.round((low + high) / 2)
+		let guess = arr[midPoint]
+
+		if (guess === element) return midPoint
+		if (guess > element) {
+			high = midPoint - 1
+		} else {
+			low = midPoint + 1
+		}
+	}
 	return `No se encontró ${element}`
 }
 
@@ -21,8 +39,15 @@ const arrCreator = itemsAmount => {
 	return arr
 }
 
-const miArr = arrCreator(100)
+const miArr = arrCreator(100000)
 console.log(miArr)
+console.time('recursive')
 console.log(binarySearch(miArr, 36))
 console.log(binarySearch(miArr, 2401))
 console.log(binarySearch(miArr, 8649))
+console.timeEnd('recursive')
+console.time('iterative')
+console.log('iterative ', binarySearchIterative(miArr, 36))
+console.log('iterative ', binarySearchIterative(miArr, 2401))
+console.log('iterative ', binarySearchIterative(miArr, 8649))
+console.timeEnd('iterative')
