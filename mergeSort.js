c68b9263485ce8759d139483d65c4a9490859f7b
Miguel Mejía <121742455+dothedada@users.noreply.github.com>
2024-05-12 19:53:13 -0500

    Limpia el codigo del merge sort

--- mergeSort.js
+++ mergeSort.js
@@ -1,27 +1,24 @@
 const mergeSort = list => {
-	if (list.length > 1) {
-		// Divide the array and introduce the recursive function
-		const midPoint = Math.ceil(list.length / 2)
-		const left = mergeSort(list.slice(0, midPoint))
-		const right = mergeSort(list.slice(midPoint))
+	if (list.length <= 1) return list
 
-		// loop over the parts values to create the new array sorted
-		const sortedArray = []
-		while (left.length && right.length) {
-			if (left[0] <= right[0]) {
-				sortedArray.push(left.shift())
-			} else {
-				sortedArray.push(right.shift())
-			}
+	// Divide the array and introduce the recursive function
+	const midPoint = Math.ceil(list.length / 2)
+	const left = mergeSort(list.slice(0, midPoint))
+	const right = mergeSort(list.slice(midPoint))
+
+	// loop over the parts values to create the new array sorted
+	const sortedArray = []
+	while (left.length && right.length) {
+		if (left[0] <= right[0]) {
+			sortedArray.push(left.shift())
+		} else {
+			sortedArray.push(right.shift())
 		}
-		// if remains any value in the left o rigth array
-		if (left.length) sortedArray.push(...left)
-		if (right.length) sortedArray.push(...right)
-
-		return sortedArray
 	}
+	// and push any remaining value in the left o rigth array
+	sortedArray.push(...left, ...right)
 
-	return list
+	return sortedArray
 }
 
 const arr = [3, 2, 14, 13, 8, 5, 1, 0]
