34d703360fd77d197a3ef57daac3986ae2bd2474
Miguel Mejía <121742455+dothedada@users.noreply.github.com>
2024-05-16 22:45:32 -0500

    limpia el archivo de selectionSort.js

--- selectionSort.js
+++ selectionSort.js
@@ -1,14 +1,4 @@
-// encuentra el valor mas alto
-//     toma el valor actual y lo compara con el siguiente
-//     se queda con el mas alto
-//     y lo compara con el siguiente
-//     hasta llegar al final del array
-//
-// lo inserta en el nuevo array
-// lo elimina del array base
-//
-// repite la operacieon hasta que el length del array base es 0
-
+// Iteración
 const getHigherValueIndex = arr => {
 	let index = 0
 	let pointer = index + 1
@@ -31,6 +21,7 @@ const seleccionSortIterative = arr => {
 	return sorted
 }
 
+// Recursión
 const selectionSortRecursion = (arr, sorted = []) => {
 	if (!arr.length) return sorted
 
@@ -48,23 +39,3 @@ const selectionSortRecursion = (arr, sorted = []) => {
 	return selectionSortRecursion(tmpArr, sortedArr)
 }
 
-const arrGen = length => {
-	const nuArr = []
-	for (let i = 0; i< length ; i++) {
-		nuArr.push(Math.round(Math.random()*length))
-	}
-	return nuArr
-}
-
-
-// const miArr = [1, 32, 53, 12, 45, 67, 53, 234, 6, 2, 6789, 3, 24, 9, 12]
-const miArr = arrGen(1000)
-console.time('iterative')
-console.log(seleccionSortIterative(miArr))
-console.timeEnd('iterative')
-console.time('recursion')
-console.log(selectionSortRecursion(miArr))
-console.timeEnd('recursion')
-console.time('sort method')
-console.log(miArr.sort((a,b) => b-a))
-console.timeEnd('sort method')
