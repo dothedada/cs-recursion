875d401af6f7fc696bb63353685f225429cfadaf
Miguel Mejía <121742455+dothedada@users.noreply.github.com>
2024-05-17 16:27:04 -0500

    Crea la clase de LinkedList y la FFactory de node

--- linkedList.js
+++ linkedList.js
@@ -14,7 +14,14 @@ class LinkedList {
 		this.value = tmpValues.pop()
 		this.next = links
 	}
+
+	append(value) {
+		if (!this.next) this.next = node(value)
+	}
 }
 
 const list = new LinkedList(1, 2, 3, 4, 5, 6, 7, 8)
-console.log(JSON.stringify(list,null, 2))
+const mini = new LinkedList('a')
+mini.append('b')
+console.log(mini)
+console.log(JSON.stringify(list, null, 2))
