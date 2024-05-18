e71657ef745787b19208a8f9668505457f51bd64
Miguel Mejía <121742455+dothedada@users.noreply.github.com>
2024-05-17 19:51:00 -0500

    Convierte Node en clase y linkedList en extensión de Node para mejorar la consistencia del objeto

--- linkedList.js
+++ linkedList.js
@@ -8,10 +8,36 @@ class Node {
 class LinkedList extends Node {
 	constructor(head, ...values) {
 		super(head)
-		values.forEach(value => this.append(value))
+		this.append(values)
+		// values.forEach(value => this.append(value))
 	}
 
-	get head() {
+	append(...values) {
+		const tmpValues = values
+		let tmpNodes
+
+		console.log(tmpValues)
+
+
+		// this.tail().next = new Node(value)
+	}
+
+	prepend(value) {
+		this.next = { ...this }
+		this.value = value
+	}
+
+	get size() {
+		let tmpNode = this
+		let listSize = 0
+		while (tmpNode.next) {
+			listSize += 1
+			tmpNode = tmpNode.next
+		}
+		return listSize + 1
+	}
+
+	head() {
 		return this.value
 	}
 
@@ -20,26 +46,8 @@ class LinkedList extends Node {
 		while (tmpNode.next) tmpNode = tmpNode.next
 		return tmpNode
 	}
-	//
-	// get size() {
-	// 	let tmpNode = this
-	// 	let listSize = 0
-	// 	while (tmpNode.next) {
-	// 		listSize += 1
-	// 		tmpNode = tmpNode.next
-	// 	}
-	// 	return listSize + 1
-	// }
-	//
-	append(value) {
 
-		this.tail().next = new Node(value)
-	}
-	//
-	prepend(value) {
-		this.next = { ...this }
-		this.value = value
-	}
+
 }
 
 const list = new LinkedList(1, 2, 3, 4, 5, 6, 7, 8)
@@ -47,4 +55,5 @@ console.log(JSON.stringify(list, null, 2))
 console.log(list.value, list.next)
 list.prepend(0)
 console.log(JSON.stringify(list, null, 2))
-const mini = new LinkedList('a')
+const mini = new LinkedList('a', 1,2,5,4,56,3)
+
