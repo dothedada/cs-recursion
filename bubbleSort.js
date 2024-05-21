d1ffb227a8bb2a7cf90b2fcc5d9535ba72bb173f
Miguel Mejía <121742455+dothedada@users.noreply.github.com>
2024-05-21 14:45:26 -0500

    Limpia codigo

--- bubbleSort.js
+++ bubbleSort.js
@@ -1,28 +1,11 @@
 const bubbleSort = list => {
 	if (list.length === 1) return list
-	let left = 0
-	let rigth = 1
-	for (let i = 0; i < list.length -1; i++) {
-		if (list[i] <= list[i+1]) {
-			[list[i], list[i+1]] = [list[i+1], list[i]];
+
+	for (let i = 0; i < list.length - 1; i++) {
+		if (list[i] <= list[i + 1]) {
+			[list[i], list[i + 1]] = [list[i + 1], list[i]];
 		}
 	}
-	// while (rigth < list.length) {
-	// 	if (list[rigth] >= list[left]) {
-	// 		[list[left], list[rigth]] = [list[rigth], list[left]];
-	// 	}
-	// 	left++
-	// 	rigth++
-	// }
+
 	return [list.pop(), ...bubbleSort(list)]
 }
-
-const miArr = (() => {
-	const arr = []
-	for (let i = 0; i <100 ; i++){
-		arr.push(Math.round(Math.random()* 100))
-	}
-	return arr
-
-})()
-console.log(bubbleSort(miArr))
