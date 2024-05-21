d054641ec29079aec223a900d6bc1f9c62e83f1c
Miguel Mejía <121742455+dothedada@users.noreply.github.com>
2024-05-21 14:44:11 -0500

    Reduce lacantidad de variables empleadas y el iterador en bubble sorth

--- bubbleSort.js
+++ bubbleSort.js
@@ -1,16 +1,28 @@
 const bubbleSort = list => {
 	if (list.length === 1) return list
-
 	let left = 0
 	let rigth = 1
-
-	while (rigth < list.length) {
-		if (list[rigth] >= list[left]) {
-			[list[left], list[rigth]] = [list[rigth], list[left]];
+	for (let i = 0; i < list.length -1; i++) {
+		if (list[i] <= list[i+1]) {
+			[list[i], list[i+1]] = [list[i+1], list[i]];
 		}
-		left++
-		rigth++
 	}
-
+	// while (rigth < list.length) {
+	// 	if (list[rigth] >= list[left]) {
+	// 		[list[left], list[rigth]] = [list[rigth], list[left]];
+	// 	}
+	// 	left++
+	// 	rigth++
+	// }
 	return [list.pop(), ...bubbleSort(list)]
 }
+
+const miArr = (() => {
+	const arr = []
+	for (let i = 0; i <100 ; i++){
+		arr.push(Math.round(Math.random()* 100))
+	}
+	return arr
+
+})()
+console.log(bubbleSort(miArr))
