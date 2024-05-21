90c0a3f424b802e8efa5a13c4b5ae93b9027c375
Miguel Mejía <121742455+dothedada@users.noreply.github.com>
2024-05-21 12:41:51 -0500

    Cambia el retorno de destructuración por concatenado, Realiza comentarios en los otros metodos de sort

--- quickSort.js
+++ quickSort.js
@@ -1,9 +1,13 @@
 const quickSort = list => {
 	if (list.length <= 1) return list
+
+	// create a tmp copy of the array, select a pivot and make the left and right containing arrays 
 	const tmpArr = [...list]
 	const pivot = tmpArr.pop()
 	const left = []
 	const right = []
+
+	// Loop over the temp array and put the elements in the containing arrays
 	while (tmpArr.length) {
 		const current = tmpArr.pop()
 		if (current <= pivot) {
@@ -12,16 +16,7 @@ const quickSort = list => {
 			right.push(current)
 		}
 	}
-	return [...quickSort(left), pivot, ...quickSort(right)]
-}
 
-const arrCreator = itemsAmount => {
-	const arr = []
-	for (let i = 0; i < itemsAmount; i++) arr.push(i)
-	arr.sort(() => Math.random()*2-1)
-	return arr
+	// merge the sorted arrays, could use destructuring arg, but concat seems more performant
+	return quickSort(left).concat(pivot, ...quickSort(right))
 }
-const miArr = arrCreator(100)
-const arrangedList = quickSort(miArr)
-console.log(miArr)
-console.log(arrangedList)
