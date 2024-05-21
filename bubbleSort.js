74b2af1726e218547091fc66cd787bc68a72b82a
Miguel Mejía <121742455+dothedada@users.noreply.github.com>
2024-05-21 14:50:48 -0500

    Evita que bubbleSort altere el array original

--- bubbleSort.js
+++ bubbleSort.js
@@ -1,11 +1,21 @@
-const bubbleSort = list => {
+const bubbleSort = arr => {
+	const list = [...arr]
 	if (list.length === 1) return list
 
-	for (let i = 0; i < list.length - 1; i++) {
-		if (list[i] <= list[i + 1]) {
-			[list[i], list[i + 1]] = [list[i + 1], list[i]];
+	for (let i = 0; i < list.length -1; i++) {
+		if (list[i] <= list[i+1]) {
+			[list[i], list[i+1]] = [list[i+1], list[i]];
 		}
 	}
-
 	return [list.pop(), ...bubbleSort(list)]
 }
+
+const miArr = (() => {
+	const arr = []
+	for (let i = 0; i <100 ; i++){
+		arr.push(Math.round(Math.random()* 100))
+	}
+	return arr
+
+})()
+console.log(bubbleSort(miArr))
