26f3597ba8641c54fc7121d3f571f68c2c5df0f3
Miguel Mejía <121742455+dothedada@users.noreply.github.com>
2024-05-22 15:51:48 -0500

    Crea la clase hashMap y exporta el linkedlist

--- hashMap.js
+++ hashMap.js
@@ -8,7 +8,19 @@ class HashMap {
 			this.data[i] = new LinkedList()
 		}
 	}
+
+	hash(key) {
+		let hashCode = 0;
+
+		const primeNumber = 31;
+		for (let i = 0; i < key.length; i++) {
+			hashCode = primeNumber * hashCode + key.charCodeAt(i);
+		}
+
+		return hashCode;
+	}
 }
 const pato = new HashMap(5)
 console.log(pato)
+console.log(pato.hash('Miguel'))
 
