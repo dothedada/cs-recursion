ff7ab54792e2187cf6ce9a2f10513c8fb5d4a463
Miguel Mejía <121742455+dothedada@users.noreply.github.com>
2024-05-22 20:50:37 -0500

    Crea el método remove

--- hashMap.js
+++ hashMap.js
@@ -6,7 +6,7 @@ class HashMap {
 		this.data = new Array(buckets)
 	}
 
-	hash(key) {
+	#hash(key) {
 		let hashCode = 0
 
 		const primeNumber = 31
@@ -20,7 +20,7 @@ class HashMap {
 	// Método de auto grow o shrink
 
 	set(key, value) {
-		const hash = this.hash(key)
+		const hash = this.#hash(key)
 		if (!this.data[hash]) {
 			this.data[hash] = [{ key, value }]
 		} else if (this.data[hash].some(e => e.key === key)) {
@@ -31,23 +31,37 @@ class HashMap {
 	}
 
 	get(key) {
-		const hash = this.hash(key)
+		const hash = this.#hash(key)
 		if (!this.data[hash]) return null
 		if (this.data[hash].length === 1) return this.data[hash][0].value
 		return this.data[hash].find(e => e.key === key).value
 	}
 
 	has(key) {
-		const hash = this.hash(key)
+		const hash = this.#hash(key)
 		if (!this.data[hash]) return false
 		return this.data[hash].some(e => e.key === key)
 	}
-}
-const pato = new HashMap(1100)
-pato.set('miguel', 1234)
-pato.set('manuel', 6789)
-console.log(JSON.stringify(pato, null, 2))
-console.log(pato.get('miguel'))
-console.log(pato.has('manuel'))
-console.log(pato.has('vic'))
 
+	remove(key) {
+		const hash = this.#hash(key)
+		if (!this.data[hash]) return false
+		const index = this.data[hash].findIndex(e => e.key === key)
+		this.data[hash].splice(index, 1)
+
+		return true
+
+	}
+}
+const pato = new HashMap(1)
+pato.set('miguel', 'prueba miguel')
+pato.set('marcela', 'prueba manuel')
+pato.set('maria', 'Prueba mario')
+console.log(JSON.stringify(pato, null, 2))
+console.log(pato.remove('marcela'))
+console.log(JSON.stringify(pato, null, 2))
+// console.log(JSON.stringify(pato, null, 2))
+// console.log(pato.get('miguel'))
+// console.log(pato.has('manuel'))
+// console.log(pato.has('vic'))
+//
