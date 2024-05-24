29f74aebdba7e39bb233db8dee6b5fe4d7b4aeee
Miguel Mejía <121742455+dothedada@users.noreply.github.com>
2024-05-23 21:46:42 -0500

    Crea el metodo find y delete item(cuando es leave o solo tiene un hijo)

--- binarySearchTrees.js
+++ binarySearchTrees.js
@@ -21,10 +21,11 @@ class BSTtree extends BSTnode {
 		return quickSort(arr.filter((value, i) => i === arr.indexOf(value)))
 	}
 
-	#getParent(value, arr) {
-		if (arr.left.value === value || arr.right.value === value) return arr
-		if (arr.value < value) return this.#getParent(value, arr.right)
-		if (arr.value > value) return this.#getParent(value, arr.left)
+	#getParentChild(value, arr) {
+		if (arr?.left?.value === value) return [arr, arr.left]
+		if (arr?.right?.value === value) return [arr, arr.right]
+		if (arr?.value < value) return this.#getParentChild(value, arr.right)
+		if (arr?.value > value) return this.#getParentChild(value, arr.left)
 		return null
 	}
 
@@ -41,17 +42,43 @@ class BSTtree extends BSTnode {
 	insert(value) {
 		const findParent = (arr) => {
 			if (value === arr.value) return
-			if (value < arr.value) 
+			if (value < arr.value)
 				return !arr.left ? arr.left = new BSTnode(value) : findParent(arr.left)
-			if (value > arr.value) 
+			if (value > arr.value)
 				return !arr.right ? arr.right = new BSTnode(value) : findParent(arr.right)
 		}
 		findParent(this)
 	}
-	
-	deleteItem(value) {
-		return this.#getParent(value, this)
 
+	deleteItem(value) {
+		const parentChild = this.#getParentChild(value, this)
+		if (!parentChild) return false
+
+		const [parent, child] = [...parentChild]
+		if (child.left && child.right) return 'ambos'
+
+		if (!child.left && !child.right) {
+			if (parent.value < value) {
+				parent.right = null
+			} else {
+				parent.left = null
+			}
+			return true
+		}
+
+		if (parent.value < value) {
+			parent.right = child.right ? child.right : child.left
+		} else {
+			parent.left = child.right ? child.right : child.left
+		}
+		return true
+
+	}
+
+	find(value) {
+		const nodes = this.#getParentChild(value, this)
+		if (!nodes) return null
+		return nodes[1]
 	}
 
 }
@@ -71,7 +98,9 @@ const prettyPrint = (node, prefix = "", isLeft = true) => {
 const pato = new BSTtree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324])
 // const pato = new BSTtree([1, 324])
 // prettyPrint(pato)
+pato.insert(6)
+console.log(pato.deleteItem(23))
+console.log(pato.find(5))
 prettyPrint(pato)
-console.log(pato.deleteItem(1))
 
 
