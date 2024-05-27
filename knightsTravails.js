d9063340fdf00acf5b3014b00414505d39043d37
Miguel Mejía <121742455+dothedada@users.noreply.github.com>
2024-05-26 19:43:33 -0500

    Mejora la lectura del código

--- knightsTravails.js
+++ knightsTravails.js
@@ -2,33 +2,26 @@ const board = (() => {
     return new Array(8).fill(null).map(() => new Array(8).fill(''));
 })();
 
-const posibleNextMoves = (arr) => {
-    const [knightX, knightY] = [...arr];
-    const moves = [
-        [2, 1],
-        [2, -1],
-        [-2, 1],
-        [-2, -1],
-        [1, 2],
-        [1, -2],
-        [-1, 2],
-        [-1, -2],
+const getMoves = (position) => {
+    const [knightX, knightY] = [...position];
+    const knightMoves = [
+        [2, 1], [2, -1], [-2, 1], [-2, -1],
+        [1, 2], [1, -2], [-1, 2], [-1, -2],
     ];
 
-    const posibleMoves = moves
+    const movesInBoard = knightMoves
         .map(([x, y]) => [knightX + x, knightY + y])
         .filter(([x, y]) => x >= 0 && x < 8 && y >= 0 && y < 8);
 
     const allowedMoves = [];
-
-    for (const move of posibleMoves) {
+    for (const move of movesInBoard) {
         if (!board[move[0]][move[1]]) allowedMoves.push(move);
     }
 
     return allowedMoves;
 };
 
-class MovesHistory {
+class MovesLog {
     constructor(root = []) {
         this[root] = [];
     }
@@ -39,32 +32,30 @@ class MovesHistory {
 }
 
 const endMessage = (moves, end) => {
-	const stepsTaken = () => {
-		if (!moves.length) return `[${end}].`
-		return `[${moves.pop()}], ` + stepsTaken()
-	}
+    const stepsTaken = () => {
+        if (!moves.length) return `[${end}].`;
+        return `[${moves.pop()}], ` + stepsTaken();
+    };
     console.log(`Done in ${moves.length} moves!`);
     console.log(`This was the path: ${stepsTaken()}`);
 };
 
 const knightMoves = (start, end) => {
-    const [endX, endY] = [...end];
-
     const [startX, startY] = [...start];
-    const moves = new MovesHistory([startX, startY]);
-
+    const moves = new MovesLog([startX, startY]);
     const queue = [[startX, startY]];
+	const [endX, endY] = [...end];
+
     while (queue.length) {
         const [evalX, evalY] = [...queue.shift()];
 
-        moves.add([evalX, evalY], posibleNextMoves([evalX, evalY]));
+        moves.add([evalX, evalY], getMoves([evalX, evalY]));
         if (evalX === endX && evalY === endY) break;
-
         board[evalX][evalY] = 'visited';
-        queue.push(...posibleNextMoves([evalX, evalY]));
+        queue.push(...getMoves([evalX, evalY]));
     }
 
-	endMessage(moves[end], end)
+    endMessage(moves[end], end);
 };
 
-knightMoves([3, 2], [7, 6]);
+knightMoves([2, 0], [4, 7]);
