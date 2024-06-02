e8a6e1a3d0d6fe26b5929d30bc6c5df241a049f9
Miguel Mejía <121742455+dothedada@users.noreply.github.com>
2024-06-02 10:53:57 -0500

    feat: Superset method

--- FEM/sets.js
+++ FEM/sets.js
@@ -1,45 +1,31 @@
-const grupoA = new Set(['a', 'b', 'c', 'd']);
-const grupoB = new Set(['b', 'd', 'e', 'f']);
-
 const union = (setA, setB) => new Set([...setA, ...setB]);
 
-const instersect = (setA, setB) => {
-    return new Set([...setA].filter((element) => setB.has(element)));
+const intersection = (setA, setB) => {
+    return new Set([...setA].filter((e) => setB.has(e)));
 };
 
 const difference = (setA, setB) => {
-    return new Set([...setA].filter((element) => !setB.has(element)));
+    return new Set([...setA].filter((e) => !setB.has(e)));
 };
 
 const symetricDifference = (setA, setB) => {
-    return new Set([
-        ...[...setA]
-            .filter((element) => !setB.has(element))
-            .concat([...setB].filter((element) => !setA.has(element))),
-    ]);
-};
-
-const symetricDifferenceB = (setA, setB) => {
     const union = new Set([...setA, ...setB]);
     const intersection = new Set([...setA].filter((e) => setB.has(e)));
     return new Set([...union].filter((e) => !intersection.has(e)));
 };
 
-const grupoC = new Set([2, 4, 6, 8, 10, 12, 14]);
-const grupoD = new Set([4, 8, 12]);
-
 const isSubSet = (setA, ofSetB) => {
-    let isSubset = true;
-    setA.forEach((e) => {
-        if (!ofSetB.has(e)) isSubset = false;
-    });
-    return isSubset
+    const set = [...setA]
+    while (set.length) {
+        if(!ofSetB.has(set.pop())) return false
+    }
+    return true
 };
 
-console.log(union(grupoA, grupoB));
-console.log(instersect(grupoA, grupoB));
-console.log(symetricDifference(grupoA, grupoB));
-console.log(symetricDifferenceB(grupoA, grupoB));
-
-console.log(isSubSet(grupoC, grupoD))
-console.log(isSubSet(grupoD, grupoC))
+const isSuperSet = (setA, ofSetB) => {
+    const ofSet = [...ofSetB]
+    while (ofSet.length) {
+        if(!setA.has(ofSet.pop())) return false
+    }
+    return true
+};
