0b40fe3ceabb447463a60f6bb6ab028d728ef43f
Miguel Mejía <121742455+dothedada@users.noreply.github.com>
2024-06-02 10:19:29 -0500

    Incorpora diferenctes formas de aboradar la diferencia simetrica

--- FEM/sets.js
+++ FEM/sets.js
@@ -1,21 +1,38 @@
-const grupoA = new Set(['a', 'b', 'c', 'd'])
-const grupoB = new Set(['b', 'd', 'e', 'f'])
+const grupoA = new Set(['a', 'b', 'c', 'd']);
+const grupoB = new Set(['b', 'd', 'e', 'f']);
 
-const union = (setA, setB) => new Set([...setA, ...setB])
+const union = (setA, setB) => new Set([...setA, ...setB]);
 
 const instersect = (setA, setB) => {
-    return new Set([...setA].filter(element => setB.has(element)))
-}
+    return new Set([...setA].filter((element) => setB.has(element)));
+};
 
 const difference = (setA, setB) => {
-    return new Set([...setA].filter(element => !setB.has(element)))
-}
+    return new Set([...setA].filter((element) => !setB.has(element)));
+};
 
 const symetricDifference = (setA, setB) => {
-    const differenceA = new Set([...setA].filter(element => !setB.has(element)))
-    const differenceB = new Set([...setB].filter(element => !setA.has(element)))
-    return new Set([...differenceA, ...differenceB])
+    return new Set([
+        ...[...setA]
+            .filter((element) => !setB.has(element))
+            .concat([...setB].filter((element) => !setA.has(element))),
+    ]);
+};
+
+const symetricDifferenceB = (setA, setB) => {
+    const union = new Set([...setA, ...setB])
+    const intersection = new Set([...setA].filter(e => setB.has(e)))
+    return new Set([...union].filter(e => !intersection.has(e)))
 }
-console.log(union(grupoA, grupoB))
-console.log(instersect(grupoA, grupoB))
-console.log(symetricDifference(grupoA, grupoB))
+
+
+console.time('bla')
+console.log(union(grupoA, grupoB));
+console.log(instersect(grupoA, grupoB));
+console.timeEnd('bla')
+console.time('destruct')
+console.log(symetricDifference(grupoA, grupoB));
+console.timeEnd('destruct')
+console.time('group')
+console.log(symetricDifferenceB(grupoA, grupoB));
+console.timeEnd('group')
