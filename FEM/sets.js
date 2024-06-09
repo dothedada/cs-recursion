0f5c8322e5c9fcae836288f9ab4587d6cf5fb9d2
Miguel Mejía <121742455+dothedada@users.noreply.github.com>
2024-06-09 08:44:46 -0500

    Crea los metodos para diferencias y relacion de sets, limpia el código

--- FEM/sets.js
+++ FEM/sets.js
@@ -1,56 +1,36 @@
-const union = (setA, setB) => new Set([...setA, ...setB]);
-
-const intersection = (setA, setB) => {
-    return new Set([...setA].filter((e) => setB.has(e)));
-};
-
-const difference = (setA, setB) => {
-    return new Set([...setA].filter((e) => !setB.has(e)));
-};
-
-const symetricDifference = (setA, setB) => {
-    const union = new Set([...setA, ...setB]);
-    const intersection = new Set([...setA].filter((e) => setB.has(e)));
-    return new Set([...union].filter((e) => !intersection.has(e)));
-};
-
-const isSubSet = (setA, ofSetB) => {
-    const set = [...setA]
-    while (set.length) {
-        if(!ofSetB.has(set.pop())) return false
-    }
-    return true
-};
-
-const isSuperSet = (setA, ofSetB) => {
-    const ofSet = [...ofSetB]
-    while (ofSet.length) {
-        if(!setA.has(ofSet.pop())) return false
-    }
-    return true
-};
-
-// FUNCIONA, pero no es práctica recomendada, 
-// es mejor crear una clase derivada
-// 
-// Set.prototype.intersection = function (setB) {
-//     return new Set([...this].filter(e => setB.has(e)))
-// }
-
 class MySet extends Set {
-    union (setB) {
-        return new MySet([...this, ...setB])
-    } 
+    union(setB) {
+        return new MySet([...this, ...setB]);
+    }
 
-    interseccion (setB) {
-        return new MySet([...this].filter(e => setB.has(e)))
+    interseccion(setB) {
+        return new MySet([...this].filter((e) => setB.has(e)));
+    }
+
+    difference(setB) {
+        return new MySet([...this].filter((e) => !setB.has(e)));
+    }
+
+    symetricDifference(setB) {
+        return new MySet(
+            [...this, ...setB].filter((e) => !this.interseccion(setB).has(e)),
+        );
+    }
+
+    isSubset(setB){
+        const set = [...this]
+
+        while (set.length) {
+            if (!setB.has(set.pop())) return false
+        }
+        return true
+    }
+
+    isSuperset(setB){
+        const set = [...setB]
+        while (set.length) {
+            if (!this.has(set.pop())) return false
+        }
+        return true
     }
 }
-
-const setA = new MySet([1,2,3,4,5])
-const setB = new Set([2,4,6,8])
-
-// setA.intersection(setB)
-console.log('Interseccion: ', setA.interseccion(setB))
-console.log('Union: ', setA.union(setB))
-console.log('Set a: ', setA, 'Set b: ', setB)
