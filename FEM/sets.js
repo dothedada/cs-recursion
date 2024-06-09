6dd7ff37c383e7508a14020f88a37e6856d866d2
Miguel Mejía <121742455+dothedada@users.noreply.github.com>
2024-06-09 08:19:08 -0500

    Crea MySet para albergar los metodos como parte de la clase, incorpora union e interseccion

--- FEM/sets.js
+++ FEM/sets.js
@@ -29,3 +29,28 @@ const isSuperSet = (setA, ofSetB) => {
     }
     return true
 };
+
+// FUNCIONA, pero no es práctica recomendada, 
+// es mejor crear una clase derivada
+// 
+// Set.prototype.intersection = function (setB) {
+//     return new Set([...this].filter(e => setB.has(e)))
+// }
+
+class MySet extends Set {
+    union (setB) {
+        return new MySet([...this, ...setB])
+    } 
+
+    interseccion (setB) {
+        return new MySet([...this].filter(e => setB.has(e)))
+    }
+}
+
+const setA = new MySet([1,2,3,4,5])
+const setB = new Set([2,4,6,8])
+
+// setA.intersection(setB)
+console.log('Interseccion: ', setA.interseccion(setB))
+console.log('Union: ', setA.union(setB))
+console.log('Set a: ', setA, 'Set b: ', setB)
